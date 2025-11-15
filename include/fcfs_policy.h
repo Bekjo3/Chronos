18116a6ff882ea0558d3ca3d4b460a998ac26dd6
Bereket Wolderufael <bereket.wolderufael@uconn.edu>
2025-11-15 15:25:56 -0500

    add compare all feature

--- include/fcfs_policy.h
+++ include/fcfs_policy.h
@@ -2,25 +2,25 @@
 #define CHRONOS_FCFS_POLICY_H
 
 #include "scheduling_policy.h"
-#include "string"
-#include "vector"
+#include <string>
+#include <vector>
 
 namespace chronos {
 
-class FCFS : public ISchedulingPolicy {
-    public:
-        ~FCFSPolicy() override = default;
+class FCFSPolicy final : public ISchedulingPolicy {
+public:
+    ~FCFSPolicy() override = default;
 
-        // Selects the job with the earliest arrival time.
-        // Tie-breaker: smaller job_id.
-        Job* getNextJob(std::vector<Job>& ready_queue) override;
+    // Selects the job with the earliest arrival time.
+    // Tie-breaker: smaller job_id.
+    Job* getNextJob(std::vector<Job>& ready_queue) override;
 
-        // FCFS has no special completion handling --> just moves on to the next one.
-        void onJobCompletion(Job* /*completed_job*/, float /*current_time*/) override {}
+    // FCFS has no special completion handling --> just moves on to the next one.
+    void onJobCompletion(Job* /*completed_job*/, float /*current_time*/) override {}
 
-        std::string getName() const override { return "FCFS"; }
+    std::string getName() const override { return "FCFS"; }
 
-        bool isPreemptive() const override { return false; }
+    bool isPreemptive() const override { return false; }
 };
 
 }
